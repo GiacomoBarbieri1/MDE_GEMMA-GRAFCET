907faa806c18459f6be68d02eaeba198e615a933
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2020-09-18 23:50:28 -0500

    initial boolean expresion parsing

--- src/properties/properties-view.tsx
+++ src/properties/properties-view.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import { Resizable } from "re-resizable";
-import React from "react";
+import React, { useState } from "react";
 import { resizableEnable } from "../utils";
 import { PropertiesTableNode } from "./properties-table";
 import { useStore } from "../App";
@@ -12,6 +12,8 @@ type Props = {};
 
 export const PropertiesView: React.FC<Props> = observer(() => {
   const rootStore = useStore();
+  const [t, setT] = useState<string>("");
+
   let inner;
   if (rootStore.selectedNode != null) {
     const selectedNode = rootStore.selectedNode;
@@ -69,9 +71,59 @@ export const PropertiesView: React.FC<Props> = observer(() => {
       </div>
     );
   } else {
+    const _tokens = tokens(t);
+    console.log(_tokens);
+    let prevIndex = 0;
     inner = (
       <div style={{ width: "150px" }} className="center">
-        Not Selected
+        <span
+          style={{
+            font: "400 13.3333px monospace",
+            position: "absolute",
+            top: 0,
+            margin: "3px",
+            width: "180px",
+          }}
+        >
+          {_tokens.map(([c, textIndex], index) => {
+            let color: string;
+            if (c instanceof VarId) {
+              color = "black";
+            } else if (["AND", "NOT", "OR"].includes(c)) {
+              color = "blue";
+            } else {
+              color = "brown";
+            }
+            const whiteSpace = textIndex - prevIndex;
+            console.log(whiteSpace);
+            const text =
+              (whiteSpace !== 0 ? " ".repeat(whiteSpace) : "") + c.toString();
+            prevIndex = textIndex + c.toString().length;
+            return (
+              <span key={index} style={{ color }}>
+                {text}
+              </span>
+            );
+          })}
+        </span>
+        <textarea
+          style={{
+            font: "400 13.3333px monospace",
+            color: "transparent",
+            caretColor: "black",
+            width: "180px",
+            position: "relative",
+            background: "transparent",
+          }}
+          value={t}
+          onSelect={(e) => {
+            console.log(e.currentTarget.selectionStart);
+          }}
+          onChange={(e) => {
+            setT(e.currentTarget.value);
+            // console.log(tokens(e.currentTarget.value));
+          }}
+        ></textarea>
       </div>
     );
   }
@@ -92,3 +144,104 @@ export const PropertiesView: React.FC<Props> = observer(() => {
     </Resizable>
   );
 });
+
+class VarId {
+  constructor(public text: string) {}
+
+  toString(): string {
+    return this.text;
+  }
+}
+type Token =
+  | "("
+  | ")"
+  | "AND"
+  | "OR"
+  | "NOT"
+  | "<"
+  | ">"
+  | "="
+  | "<="
+  | ">="
+  | VarId;
+
+const tokens = (t: string): [Token, number][] => {
+  const l: [Token, number][] = [];
+  let i = -1;
+  let omit = 0;
+  let signal = "";
+  const addSignal = () => {
+    if (signal.length !== 0) {
+      l.push([new VarId(signal), i - signal.length]);
+      signal = "";
+    }
+  };
+  const add = (v: Token) => {
+    addSignal();
+    l.push([v, i]);
+  };
+
+  for (const c of t) {
+    i++;
+    if (omit !== 0) {
+      omit--;
+      continue;
+    }
+    switch (c) {
+      case " ":
+        addSignal();
+        break;
+      case ")":
+      case "(":
+      case "<":
+      case ">":
+        add(c);
+        break;
+      case "=":
+        const prev = l[l.length - 1][0];
+        const strPrev = prev instanceof VarId ? prev.text : prev;
+        if (["<", ">"].includes(strPrev)) {
+          l[l.length - 1][0] = (prev + "=") as any;
+        } else {
+          add(c);
+        }
+        break;
+      case "A":
+        if (t.substring(i, i + 3) == "AND") {
+          omit = 2;
+          add("AND");
+          continue;
+        }
+      case "O":
+        if (t.substring(i, i + 2) == "OR") {
+          omit = 1;
+          add("OR");
+          continue;
+        }
+      case "N":
+        if (t.substring(i, i + 3) == "NOT") {
+          omit = 2;
+          add("NOT");
+          continue;
+        }
+
+      default:
+        signal += c;
+        break;
+    }
+  }
+  if (signal.length !== 0) {
+    l.push([new VarId(signal), i - signal.length + 1]);
+  }
+  return l;
+};
+
+const term = (t: string) => {
+  for (const c of t) {
+  }
+};
+
+const parseBool = (t: string) => {
+  for (const c of t) {
+  }
+};
